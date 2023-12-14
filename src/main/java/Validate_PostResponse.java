c290711111f15fafc13f915b54cc8624a6f48003
Badhrusamy <samybadhru1994@gmail.com>
2023-12-14 11:41:38 +0530

    Changes

--- src/main/java/Validate_PostResponse.java
+++ src/main/java/Validate_PostResponse.java
@@ -4,101 +4,32 @@ import io.restassured.response.Response;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import static io.restassured.RestAssured.given;
-import static org.testng.Assert.assertEquals;
-
-public class Validate_POSTResponse{
-
-
+public class Validate_POSTResponse {
     @Test
-    public void validateResponse ()
-    {
-        try {
-            // Specify the URL of the API endpoint
-            String apiUrl = "http://restapi.adequateshop.com/api/Tourist";
+    public void validateResponse() {
+        // Set the base URI for the API
+        RestAssured.baseURI = "http://restapi.adequateshop.com/api/Tourist";
 
-            // Create a URL object
-            URL url = new URL(apiUrl);
+        // Define the payload
+        String payload = "{\n" +
+                "\"tourist_name\": \"Badhrusamy\",\n" +
+                "\"tourist_email\": \"Badhrusamysundaram@idealtechlabs.com\",\n" +
+                "\"tourist_location\": \"Paris\"\n" +
+                "}";
 
-            // Open a connection to the URL
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        // Send a POST request with the payload and capture the response
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .post("/Tourist")
+                .then()
+                .statusCode(201) // Adjust the expected status code based on your API's behavior
+                .extract()
+                .response();
 
-            // Set the request method to POST
-            connection.setRequestMethod("POST");
-
-            // Set the content type and indicate that you will send output
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
-
-            // Create the request payload
-            String payload = "{"
-                    + "\"tourist_name\": \"Test\","
-                    + "\"tourist_email\": \"badhrusamy142350@idealtechlabs.com\","
-                    + "\"tourist_location\": \"Paris\""
-                    + "}";
-
-            // Get the output stream of the connection
-            try (OutputStream os = connection.getOutputStream()) {
-                // Write the request payload to the output stream
-                byte[] input = payload.getBytes("utf-8");
-                os.write(input, 0, input.length);
-            }
-
-            // Get the response code
-            int responseCode = connection.getResponseCode();
-
-            // Check if the response code is 201 (OK)
-            if (responseCode == 201) {
-                System.out.println("POST request successful");
-                Reporter.log("POST request successful");
-
-
-            } else {
-                // Print an error message if the response code is not 201
-                System.out.println("Failed to make POST request. Response Code: " + responseCode);
-            }
-
-//            Response response = RestAssured.given()
-//                    .contentType(ContentType.JSON)
-//                    .body(payload)
-//                   .post("apiUrl")
-//                   .then()
-//                   .statusCode(201) // Adjust the expected status code based on your API's behavior
-//                   .extract()
-//                   .response();
-            Response response = given().post("/Tourist");
-            int statusCode = response.getStatusCode();
-            String responseBody = response.getBody().asString();
-            System.out.println("Response Body: " + responseBody);
-            assertEquals(statusCode, 201, "Status code is not as expected");
-
-
-            Reporter.log(responseBody);
-            assertEquals(responseBody.contains("tourist_name"), true, "Response body doesn't contain expected value");
-
-
-            // Close the connection
-            connection.disconnect();
-
-            if(responseBody.contains("tourist_name"))
-            {
-                Reporter.log("Information Updated successfully");
-
-            }
-            else {
-                Reporter.log("Message\": \"Please try with different email address!\"");
-
-            }
-            // Validate status code
-
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-                }
-                }
+        // Retrieve and print the success response body
+        String successResponseBody = response.getBody().asString();
+        Reporter.log(successResponseBody);
+        System.out.println("Success Response Body: " + successResponseBody);
+    }
+}
